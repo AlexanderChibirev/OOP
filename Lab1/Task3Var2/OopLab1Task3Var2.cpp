05c13cc162a24720aa6af259ff4db54d9aca55f0
AlexanderChibirev <www.darmagh@mail.ru>
2016-02-11 22:37:14 +0300

    мелкие исправления, добавил проверку на ошибки и изменил алгоритм поиска определителя

--- Lab1/Task3Var2/OopLab1Task3Var2.cpp
+++ Lab1/Task3Var2/OopLab1Task3Var2.cpp
@@ -7,76 +7,30 @@
 #include <fstream>
 #include <iomanip>
 
+
 using namespace std;
 
 const int sizeOfMatrix = 3;
 
-struct DataProgram
-{
-	string nameImputFile;
-	ifstream fileImput;
 
-};
-
-int ErrorExitProgram()
+void ErrorExitProgram(string const &textError, bool &error)
 {
-	exit(1);
+	cout << textError << endl;
+	error = true;
 }
 
-void ErrorCheckingAndInitData(int argc, char** argv, DataProgram& dataProgram)
-{
-	struct stat fileSize;
-
-	if (argc != 2) {
-		cout << "Wrong amount of arguments was proposed" << endl;
-		ErrorExitProgram();
-	}
-	dataProgram.nameImputFile = argv[1];
-
-	stat(argv[1], &fileSize);
-	if (fileSize.st_size > 0 && fileSize.st_size < 2147483648)
-	{
-		cout << "file size larger than 2 GB" << endl;
-		ErrorExitProgram();
-	}
-
-	dataProgram.fileImput.open(dataProgram.nameImputFile);
-	if (!dataProgram.fileImput.is_open())
-	{
-		cout << "Failed to open " << dataProgram.nameImputFile << " for reading" << endl;
-		ErrorExitProgram();
-	}
-}
 
 float Determ(float** matrixInt, int sizeOfMatrix)
 {
-	int i, j;
 	float det = 0;
-	float** matr;
-	if (sizeOfMatrix == 1)
-	{
-		det = matrixInt[0][0];
-	}
-	else if (sizeOfMatrix == 2)
-	{
-		det = matrixInt[0][0] * matrixInt[1][1] - matrixInt[0][1] * matrixInt[1][0];
-	}
-	else
-	{
-		matr = new float*[sizeOfMatrix - 1];
-		for (i = 0; i<sizeOfMatrix; ++i)
-		{
-			for (j = 0; j<sizeOfMatrix - 1; ++j)
-			{
-				if (j<i)
-					matr[j] = matrixInt[j];
-				else
-					matr[j] = matrixInt[j + 1];
-			}
-			det += pow((float)-1, (i + j))*Determ(matr, sizeOfMatrix - 1)*matrixInt[i][sizeOfMatrix - 1];
-		}
-		delete[] matr;
-	}
+	float result1;
+	float result2;
+	float result3;
+	result1 = matrixInt[0][0] * ((matrixInt[1][1] * matrixInt[2][2]) - (matrixInt[1][2] * matrixInt[2][1]));
+	result2 = matrixInt[0][1] * ((matrixInt[1][0] * matrixInt[2][2]) - (matrixInt[1][2] * matrixInt[2][0]));
+	result3 = matrixInt[0][2] * ((matrixInt[1][0] * matrixInt[2][1]) - (matrixInt[1][1] * matrixInt[2][0]));
+	det = result1 - result2 + result3;
+	cout << det;
 	return det;
 }
 
@@ -88,16 +42,15 @@ void InverseMatrix(float** matrixInt, int sizeOfMatrix)
 
 	for (int i = 0; i < sizeOfMatrix; i++)
 		identityMatrix[i] = new float[sizeOfMatrix];
-
+	//делаем еденичную матрицу////////////////////
 	for (int i = 0; i < sizeOfMatrix; i++)
 		for (int j = 0; j < sizeOfMatrix; j++)
 		{
 			identityMatrix[i][j] = 0.0;
-
 			if (i == j)
 				identityMatrix[i][j] = 1.0;
 		}
-
+	/////////////////////////////////////////////
 	for (int k = 0; k < sizeOfMatrix; k++)
 	{
 		resultOfDivision = matrixInt[k][k];
@@ -144,57 +97,80 @@ void InverseMatrix(float** matrixInt, int sizeOfMatrix)
 	delete[] identityMatrix;
 }
 
-void BeginProgramm(DataProgram& dataProgram)
+void BeginProgramm(const string &nameImputFile, bool &error)
 {
-	string outPut;
-	string lineStr;
-	string matrixStr[sizeOfMatrix][sizeOfMatrix];
-	float **matrixInt;
-	matrixInt = new float*[sizeOfMatrix];
-	for (int i = 0; i<sizeOfMatrix; ++i)
-		matrixInt[i] = new float[sizeOfMatrix];
-	for (int i = 0; i < sizeOfMatrix; i++)
+	ifstream fileImput;
+
+	fileImput.open(nameImputFile);
+	if (!fileImput.is_open())
 	{
-		for (int j = 0; j < sizeOfMatrix; j++)
-		{
-			dataProgram.fileImput >> matrixStr[i][j];
-			if (matrixStr[i][j] == "" ) {
-				cout << "Your file empty" << endl;
-				ErrorExitProgram();
-			}
-			matrixInt[i][j] = float(atof((matrixStr[i][j]).c_str()));
-			//cout << matrixInt[i][j] << "  ";
-		}
-		//cout << endl;
+		ErrorExitProgram("Failed to open input.txt for reading\n", error);
 	}
-	if (Determ(matrixInt, sizeOfMatrix) > 0)
+	if (!error)
 	{
-		InverseMatrix(matrixInt, sizeOfMatrix);
+		string outPut;
+		string lineStr;
+		string matrixStr[sizeOfMatrix][sizeOfMatrix];
+		float **matrixInt;
+		matrixInt = new float*[sizeOfMatrix];
+		for (int i = 0; i<sizeOfMatrix; ++i)
+		{
+			matrixInt[i] = new float[sizeOfMatrix];
+		}
 		for (int i = 0; i < sizeOfMatrix; i++)
 		{
 			for (int j = 0; j < sizeOfMatrix; j++)
 			{
-				cout << setprecision(3) << matrixInt[i][j] << "  ";
+				fileImput >> matrixStr[i][j];
+				if (matrixStr[i][j] == ""  && !error)
+				{
+					ErrorExitProgram("Your file empty", error);
+				}
+				matrixInt[i][j] = float(atof((matrixStr[i][j]).c_str()));
 			}
-			cout << endl;
 		}
+		if(!error)
+		{
+			if (Determ(matrixInt, sizeOfMatrix) == 0)
+			{
+				InverseMatrix(matrixInt, sizeOfMatrix);
+				for (int i = 0; i < sizeOfMatrix; i++)
+				{
+					for (int j = 0; j < sizeOfMatrix; j++)
+					{
+						cout << setprecision(3) << matrixInt[i][j] << "  ";
+					}
+					cout << endl;
+				}
+			}
+			else
+			{
+				ErrorExitProgram("The inverse matrix can not be found, since the determinant is equal to zero", error);
+			}
+		}
+		for (int i = 0; i < sizeOfMatrix; ++i)
+			delete[] matrixInt[i];
+		delete[] matrixInt;
 	}
-	else
-	{
-		cout << "The inverse matrix can not be found, since the determinant is equal to zero" << endl;
-		ErrorExitProgram();
-	}
-	for (int i = 0; i < sizeOfMatrix; ++i)
-		delete[] matrixInt[i];
-	delete[] matrixInt;
-	dataProgram.fileImput.close();
+	fileImput.close();
 }
 
 int main(int argc, char** argv)
 {
-	DataProgram dataProgram;
-	ErrorCheckingAndInitData(argc, argv, dataProgram);
-	BeginProgramm(dataProgram);
-	system("pause");
+	bool error = false;
+	string nameImputFile = argv[1];
+	if (argc != 2)
+	{
+		ErrorExitProgram("Wrong amount of arguments was proposed\nEnter a correct arguments amount please, for example:\n'OopLab1Task3Var7.exe <input.txt> ", error);
+		return 1;
+	}
+	if (!error) 
+	{
+		BeginProgramm(nameImputFile, error);
+	}
+	if (error)
+	{
+		return 1;
+	}
 	return 0;
 }
