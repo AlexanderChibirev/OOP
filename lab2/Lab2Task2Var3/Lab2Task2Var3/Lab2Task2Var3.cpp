8982d7de82ae0076f12eb8b9be6689b604a3c605
AlexanderChibirev <www.daramagh@mail.ru>
2016-03-12 03:14:01 +0300

    add ReplaceTests without CommandLineArgumentTest

---
+++ lab2/Lab2Task2Var3/Lab2Task2Var3/Lab2Task2Var3.cpp
@@ -0,0 +1,26 @@
+// Lab2Task2Var3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+
+int _tmain(int argc, char** argv)
+{
+	//if (argc != 3)
+	//{
+	//	cout << "Wrong amount of arguments was proposed\nEnter a correct arguments amount please, for example:\n' <search string> <replace string>'";
+	//	return 1;
+	//}
+	////string subject;
+	//cout << "text enter: ";
+	//cin >> subject;
+	////string search = argv[1];
+	////string replace = argv[2];
+
+	////subject = (FindAndReplace(subject, search, replace));
+	//cout << subject;
+    return 0;
+}
+
